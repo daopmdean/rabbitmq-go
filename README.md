062513d684efbe8f451e589518845243684b115c
DaoPM Dean <daopm.dean@gmail.com>
2025-05-02 23:08:53 +0700

    hello example

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # rabbitmq-go
+
+## Installing RabbitMQ
+```
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4-management
+```
+
+Go to `localhost:15672` for management page
+
+## Running hello example
+Start the receiving
+```
+go run cmd/hello/receiving/main.go
+```
+
+Start the sending
+```
+go run cmd/hello/send/main.go
+```
---
+++ cmd/hello/receiving/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	var running chan struct{}
+
+	go func() {
+		for msg := range msgs {
+			log.Printf("Received a message: %s", msg.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-running
+
+}
